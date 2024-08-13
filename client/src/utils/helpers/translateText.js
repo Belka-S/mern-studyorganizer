5cc0c2cadcee5dc4ee97440662f6451666c1fd02
Belka <belka.srh.it@gmail.com>
2024-08-13 10:13:11 +0200

    update: translate text

--- client/src/utils/helpers/translateText.js
+++ client/src/utils/helpers/translateText.js
@@ -6,7 +6,9 @@ translate.engine = 'google'; // "google", "yandex", "libre", "deepl"
 export const translateText = async (text, { from, to }) => {
   try {
     const translation = await translate(`${text}`, { from, to });
-    return translation;
+    return translation.split(' ').length !== 1
+      ? translation
+      : translation.at(0).toLocaleLowerCase() + translation.substring(1);
   } catch (err) {
     return err.message;
   }
