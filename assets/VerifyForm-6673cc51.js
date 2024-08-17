08e5ea85c4eeeddcffab535be602f4b6c10a8f5a
Belka-S <Belka-S@users.noreply.github.com>
2024-08-17 12:40:07 +0000

    deploy: ba0db4e0654a4ad3463729a69f072a3350cb05b4

--- assets/VerifyForm-6673cc51.js
+++ assets/VerifyForm-6673cc51.js
@@ -1,4 +1,4 @@
-import{n as u,s as c,t as x,L as f,P as s,j as e,G as g,d as m,F as j,u as b,r as F,v as y,Q as C,g as v}from"./index-eaf06a76.js";import{M as $}from"./index.esm-d458f45b.js";import{f as k,F as T,a as L,T as V,L as z,E,b as S,c as w,d as M,S as O,e as R}from"./AuthForms.styled-53ef73ae.js";import{v as B}from"./clusterSchema-73151e77.js";const{colors:G}=x,l=u`
+import{n as u,s as c,t as x,L as f,P as s,j as e,G as g,d as m,F as j,u as b,r as F,v as y,Q as C,g as v}from"./index-b3ff70e7.js";import{M as $}from"./index.esm-3c9f5bdc.js";import{f as k,F as T,a as L,T as V,L as z,E,b as S,c as w,d as M,S as O,e as R}from"./AuthForms.styled-024c5103.js";import{v as B}from"./clusterSchema-1355fed6.js";const{colors:G}=x,l=u`
   margin-left: auto;
 
   color: ${G.accent};
