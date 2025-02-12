46babd23cae5659d3222f0815b94616395ebeb34
Belka-S <Belka-S@users.noreply.github.com>
2025-02-12 16:48:51 +0000

    deploy: a33fdc71bafa1d6f4f2650e1f8b03a21df5266b1

--- assets/ClusterPage-5a41fec3.js
+++ assets/ClusterPage-5a41fec3.js
@@ -1,4 +1,4 @@
-import{o as E,p as b,t as S,P as v,j as e,u as G,r as $,q as T,B as _,v as re,w as D,Q as L,x as oe,k as ne,y as P,z as ce,A as le,C as ae,M as N,D as I,H as ie,J as ue,K as de,N as pe,O as he,R as me,U as fe,W as xe,X as be,Y as ge,Z as $e,i as Ce}from"./index-2e0a7b2d.js";import{v as je,u as ve}from"./index.esm-2c9c4813.js";import{T as ke,r as ye}from"./index-747f1e66.js";import{o as Te,F as Se,L as z,I as q,C as we,g as Fe,a as Le,A as Ee}from"./ClusterAddForm-a2e6c8b6.js";const{colors:a,shadows:Ge,indents:Ae}=S,O=E`
+import{o as E,p as b,t as S,P as v,j as e,u as G,r as $,q as T,B as _,v as re,w as D,Q as L,x as oe,k as ne,y as P,z as ce,A as le,C as ae,M as N,D as I,H as ie,J as ue,K as de,N as pe,O as he,R as me,U as fe,W as xe,X as be,Y as ge,Z as $e,i as Ce}from"./index-30ec1d75.js";import{v as je,u as ve}from"./index.esm-aa06812a.js";import{T as ke,r as ye}from"./index-92d43ad0.js";import{o as Te,F as Se,L as z,I as q,C as we,g as Fe,a as Le,A as Ee}from"./ClusterAddForm-54199040.js";const{colors:a,shadows:Ge,indents:Ae}=S,O=E`
   padding-inline: 4px;
   display: grid;
   grid-column-gap: 2px;
